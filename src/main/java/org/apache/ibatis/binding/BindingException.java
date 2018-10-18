4d9f7445e27e25fd088166251d0b27b34f97a9af
xie1 <xieshanzhong66@163.com>
2018-10-18 18:04:54 +0800

    mybatis源码学习

--- src/main/java/org/apache/ibatis/binding/BindingException.java
+++ src/main/java/org/apache/ibatis/binding/BindingException.java
@@ -24,6 +24,7 @@ public class BindingException extends PersistenceException {
 
   private static final long serialVersionUID = 4300802238789381562L;
 
+
   public BindingException() {
     super();
   }
